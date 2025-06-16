192ff88dcbe2b511dfe3585aa7584871a4f188c2
ak44711 <1074832869@qq.com>
2025-06-16 20:09:22 +0800

    Initial commit

---
+++ src/main/java/org/example/demo/attenance/Attenance.java
@@ -0,0 +1,19 @@
+package org.example.demo.attenance;
+//import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class Attenance {
+    @GetMapping("/list")
+    public String hello() {
+        return "hhh";
+    }
+}

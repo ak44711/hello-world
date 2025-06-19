f2797719b7591f3d9cba48f1d0a87f8198e036e2
ak44711 <1074832869@qq.com>
2025-06-19 19:00:55 +0800

    dafa

--- src/main/java/org/example/demo/attenance/Attenance.java
+++ src/main/java/org/example/demo/attenance/Attenance.java
@@ -14,6 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Attenance {
     @GetMapping("/list")
     public String hello() {
-        return "ffff";
+        return "xxx";
     }
 }
